8c61fbe233b3392caea2f28cce262b5b411a69b8
GrzegorzGie21 <galazka.grzesiek@gmail.com>
2020-04-07 23:53:23 +0200

    Done task2:dynamic stats from landing-page branch
    Added to landing page view new method which gets stats about the total number of bags and institutions from DB and passed them to the context in the template.

--- main/views.py
+++ main/views.py
@@ -1,11 +1,21 @@
+from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
+
+from main.models import Donation, Institution
 
 
 # Create your views here.
 class LandingPage(View):
     def get(self, request):
-        return render(request, 'main/index.html')
+        bags, institutions = self.get_bags_and_institutions()
+        context = {'bags': bags.get('quantity_sum'), 'institutions': institutions}
+        return render(request, 'main/index.html', context)
+
+    def get_bags_and_institutions(self):
+        bags = Donation.objects.aggregate(Sum('quantity'))
+        institutions = Institution.objects.all().count()
+        return bags, institutions
 
 
 class LoginPage(View):
